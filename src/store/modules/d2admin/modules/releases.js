82daa91306133ae7740da6787e5e1405eb45bfff
cj <power4j@outlook.com>
2021-03-07 17:23:27 +0800

    chore: project info

--- src/store/modules/d2admin/modules/releases.js
+++ src/store/modules/d2admin/modules/releases.js
@@ -8,10 +8,8 @@ export default {
      * @param {Object} state state
      */
     versionShow () {
-      util.log.capsule('D2Admin', `v${process.env.VUE_APP_VERSION}`)
-      console.log('D2 Admin  https://github.com/d2-projects/d2-admin')
-      console.log('D2 Crud Plus   https://github.com/greper/d2-crud-plus')
-      console.log('Document  http://greper.gitee.io/d2-crud-plus/')
+      util.log.capsule('ji-admin', `v${process.env.VUE_APP_VERSION}`)
+      console.log('https://github.com/power4j/ji-boot')
       console.log('请不要吝啬您的 star，谢谢 ~')
     }
   }
